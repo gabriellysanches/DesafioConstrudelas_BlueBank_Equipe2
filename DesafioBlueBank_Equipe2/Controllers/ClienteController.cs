8928112e6dc53eb55b5815a43a986816c9997cd3
Gabrielly Sanches <gabrielly.sanches0506@gmail.com>
2021-11-29 14:05:12 -0300

    Adicionando classe Controller de Cliente

---
+++ DesafioBlueBank_Equipe2/Controllers/ClienteController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstrudelasDesafioEq2.OrderSystem.Services.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    { 
+
+        private Dictionary<Guid, string> Clientes { get; set; }
+
+        public ClienteController()
+        {
+            Clientes = new Dictionary<Guid, string>();
+
+            Clientes.Add(Guid.NewGuid(), "Carol");
+
+        }
+  
+        [HttpGet]
+        public Dictionary<Guid, string> Get()
+        {
+            return Clientes;
+        }
+
+  
+
+    }
+
+}
---
+++ DesafioBlueBank_Equipe2/Program.cs
@@ -0,0 +1,27 @@
+using ConstrudelasDesafioEq2.OrderSystem.Services.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstrudelasDesafioEq2.OrderSystem.Services.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
